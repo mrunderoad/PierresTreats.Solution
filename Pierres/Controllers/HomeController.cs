707f211de312f73b1a279573ba153b819bb897fe
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-03-25 11:48:27 -0700

    add search feature

--- Pierres/Controllers/HomeController.cs
+++ Pierres/Controllers/HomeController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Pierres.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Pierres.Controllers
 {
@@ -20,5 +22,22 @@ namespace Pierres.Controllers
       return View();
     }
 
+    public ActionResult Search(string Search)
+    {
+      var flavors = _db.Flavors.Where(flavor => (flavor.Name.Contains(Search) || (flavor.Name == Search))).ToList();
+      var treats = _db.Treats.Where(treat => (treat.Name.Contains(Search) || (treat.Name == Search))).ToList();
+      ViewBag.Flavors = flavors;
+      ViewBag.Treats = treats;
+      return View();
+    }
+
+    public ActionResult All()
+    {
+      var treats = _db.Treats.ToList();
+      var flavors = _db.Flavors.ToList();
+      ViewBag.Treats = treats;
+      ViewBag.Flavors = flavors;
+      return View();
+    }
   }
 }
